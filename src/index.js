7fad86e5a10308cb603d4e337461cb2629272b80
803142 <803142@gmail.com>
2018-09-23 14:33:46 +0300

    regular expression

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var array = [];
+  var a=b=c=D=0;
+  var x1=x2=null;
+  equation=equation.replace(/\s/g, "");
+  array=equation.match(/[^x^]\d+/g);
+  a=Number(array[0]);
+  b=Number(array[1]);
+  c=Number(array[2]);
+  D = b * b - 4 * a * c;
+  if(D > 0) {
+    x1 = Math.round((- b + Math.sqrt(D)) / (2 * a));
+    x2 = Math.round((- b - Math.sqrt(D)) / (2 * a));
+  }else if(D === 0) {
+    x1 = Math.round((- b) / (2 * a));
+  }
+  if (x1>x2) {return[x2,x1];}
+  else {return[x1,x2];};
 }
